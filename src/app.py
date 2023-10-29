0ca973bec1827867245d9445e5e7449c1c166f98
kirisakiken <bezmican96@gmail.com>
2023-10-29 21:20:00 +0300

    feat(rows): add row schema endpoint

--- src/app.py
+++ src/app.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
 
-from src.routes import store_routes, cabinet_routes
+from src.routes import store_routes, cabinet_routes, row_routes
 
 app = FastAPI()
 # Routes
 app.include_router(store_routes.router)
 app.include_router(cabinet_routes.router)
+app.include_router(row_routes.router)
