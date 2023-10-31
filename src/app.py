b5d7b5c41f853c11bb0179c83179e6883bff1040
kirisakiken <bezmican96@gmail.com>
2023-10-31 22:54:02 +0300

    fix(app): add cors middleware

--- src/app.py
+++ src/app.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.routes import store_routes, cabinet_routes, row_routes, sku_routes, brand_routes, lane_routes
 
+origins = [
+    "http://localhost:7245",
+    "https://localhost:7245",
+]
+
 app = FastAPI()
+# Cors
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 # Routes
 app.include_router(brand_routes.router)
 app.include_router(store_routes.router)
